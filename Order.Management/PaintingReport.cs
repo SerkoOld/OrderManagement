11135d1932989169998def82141f5b21f44098fd
Daniel Nieuwoudt <Daniel.Nieuwoudt@serko.com>
2020-09-09 12:21:09 +1200

    Initial commit

---
+++ Order.Management/PaintingReport.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Management
+{
+    class PaintingReport : Order
+    {
+        public int tableWidth = 73;
+        public PaintingReport(string customerName, string customerAddress, string dueDate, List<Shape> shapes)
+        {
+            base.CustomerName = customerName;
+            base.Address = customerAddress;
+            base.DueDate = dueDate;
+            base.OrderedBlocks = shapes;
+        }
+        public override void GenerateReport()
+        {
+            Console.WriteLine("\nYour painting report has been generated: ");
+            Console.WriteLine(base.ToString());
+            generateTable();
+        }
+
+        public void generateTable()
+        {
+            PrintLine();
+            PrintRow("        ", "   Red   ", "  Blue  ", " Yellow ");
+            PrintLine();
+            PrintRow("Square", base.OrderedBlocks[0].NumberOfRedShape.ToString(), base.OrderedBlocks[0].NumberOfBlueShape.ToString(), base.OrderedBlocks[0].NumberOfYellowShape.ToString());
+            PrintRow("Triangle", base.OrderedBlocks[1].NumberOfRedShape.ToString(), base.OrderedBlocks[1].NumberOfBlueShape.ToString(), base.OrderedBlocks[1].NumberOfYellowShape.ToString());
+            PrintRow("Circle", base.OrderedBlocks[2].NumberOfRedShape.ToString(), base.OrderedBlocks[2].NumberOfBlueShape.ToString(), base.OrderedBlocks[2].NumberOfYellowShape.ToString());
+            PrintLine();
+        }
+       
+        public void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+
+        public void PrintRow(params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+
+        public string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+    }
+}
---
+++ Order.Management/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Order.Management
+{
+    class Program
+    {
+        // Main entry
+        static void Main(string[] args)
+        {
+            var (customerName, address, dueDate) = CustomerInfoInput();
+
+            var orderedShapes = CustomerOrderInput();
+
+            InvoiceReport(customerName, address, dueDate, orderedShapes);
+
+            CuttingListReport(customerName, address, dueDate, orderedShapes);
+
+            PaintingReport(customerName, address, dueDate, orderedShapes);
+        }
+        
+        // Order Circle Input
+        public static Circle OrderCirclesInput()
+        {
+            Console.Write("\nPlease input the number of Red Circle: ");
+            int redCircle = Convert.ToInt32(userInput());
+            Console.Write("Please input the number of Blue Circle: ");
+            int blueCircle = Convert.ToInt32(userInput());
+            Console.Write("Please input the number of Yellow Circle: ");
+            int yellowCircle = Convert.ToInt32(userInput());
+
+            Circle circle = new Circle(redCircle, blueCircle, yellowCircle);
+            return circle;
+        }
+        
+        // Order Squares Input
+        public static Square OrderSquaresInput()
+        {
+            Console.Write("\nPlease input the number of Red Squares: ");
+            int redSquare = Convert.ToInt32(userInput());
+            Console.Write("Please input the number of Blue Squares: ");
+            int blueSquare = Convert.ToInt32(userInput());
+            Console.Write("Please input the number of Yellow Squares: ");
+            int yellowSquare = Convert.ToInt32(userInput());
+
+            Square square = new Square(redSquare, blueSquare, yellowSquare);
+            return square;
+        }
+
+        // Order Triangles Input
+        public static Triangle OrderTrianglesInput()
+        {
+            Console.Write("\nPlease input the number of Red Triangles: ");
+            int redTriangle = Convert.ToInt32(userInput());
+            Console.Write("Please input the number of Blue Triangles: ");
+            int blueTriangle = Convert.ToInt32(userInput());
+            Console.Write("Please input the number of Yellow Triangles: ");
+            int yellowTriangle = Convert.ToInt32(userInput());
+
+            Triangle triangle = new Triangle(redTriangle, blueTriangle, yellowTriangle);
+            return triangle;
+        }
+
+        // User Console Input
+        public static string userInput()
+        {
+            string input = Console.ReadLine();
+            while (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("please enter valid details");
+                input = Console.ReadLine();
+
+            }
+            return input;
+        }
+
+        // Generate Painting Report 
+        private static void PaintingReport(string customerName, string address, string dueDate, List<Shape> orderedShapes)
+        {
+            PaintingReport paintingReport = new PaintingReport(customerName, address, dueDate, orderedShapes);
+            paintingReport.GenerateReport();
+        }
+
+        // Generate Painting Report 
+        private static void CuttingListReport(string customerName, string address, string dueDate, List<Shape> orderedShapes)
+        {
+            CuttingListReport cuttingListReport = new CuttingListReport(customerName, address, dueDate, orderedShapes);
+            cuttingListReport.GenerateReport();
+        }
+
+        // Generate Invoice Report 
+        private static void InvoiceReport(string customerName, string address, string dueDate, List<Shape> orderedShapes)
+        {
+            InvoiceReport invoiceReport = new InvoiceReport(customerName, address, dueDate, orderedShapes);
+            invoiceReport.GenerateReport();
+        }
+
+        // Get customer Info
+        private static (string customerName, string address, string dueDate) CustomerInfoInput()
+        {
+            Console.Write("Please input your Name: ");
+            string customerName = userInput();
+            Console.Write("Please input your Address: ");
+            string address = userInput();
+            Console.Write("Please input your Due Date: ");
+            string dueDate = userInput();
+            return (customerName, address, dueDate);
+        }
+
+        // Get order input
+        private static List<Shape> CustomerOrderInput()
+        {
+            Square square = OrderSquaresInput();
+            Triangle triangle = OrderTrianglesInput();
+            Circle circle = OrderCirclesInput();
+
+            var orderedShapes = new List<Shape>();
+            orderedShapes.Add(square);
+            orderedShapes.Add(triangle);
+            orderedShapes.Add(circle);
+            return orderedShapes;
+        }
+    }
+}
