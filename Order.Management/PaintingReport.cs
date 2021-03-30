70beff08e53dc94ec706ade2e6e92acbd9e75e19
WilliamAlkawaz <77669116+WilliamAlkawaz@users.noreply.github.com>
2021-03-31 10:12:05 +1300

    Update PaintingReport.cs

--- Order.Management/PaintingReport.cs
+++ Order.Management/PaintingReport.cs
@@ -9,10 +9,10 @@ namespace Order.Management
         public int tableWidth = 73;
         public PaintingReport(string customerName, string customerAddress, string dueDate, List<Shape> shapes)
         {
-            base.CustomerName = customerName;
-            base.Address = customerAddress;
-            base.DueDate = dueDate;
-            base.OrderedBlocks = shapes;
+            CustomerName = customerName;
+            Address = customerAddress;
+            DueDate = dueDate;
+            OrderedBlocks = shapes;
         }
         public override void GenerateReport()
         {
@@ -26,9 +26,9 @@ namespace Order.Management
             PrintLine(new string('-', tableWidth));
             PrintRow("        ", "   Red   ", "  Blue  ", " Yellow ");
             PrintLine(new string('-', tableWidth));
-            PrintRow("Square", base.OrderedBlocks[0].NumberOfRedShape.ToString(), base.OrderedBlocks[0].NumberOfBlueShape.ToString(), base.OrderedBlocks[0].NumberOfYellowShape.ToString());
-            PrintRow("Triangle", base.OrderedBlocks[1].NumberOfRedShape.ToString(), base.OrderedBlocks[1].NumberOfBlueShape.ToString(), base.OrderedBlocks[1].NumberOfYellowShape.ToString());
-            PrintRow("Circle", base.OrderedBlocks[2].NumberOfRedShape.ToString(), base.OrderedBlocks[2].NumberOfBlueShape.ToString(), base.OrderedBlocks[2].NumberOfYellowShape.ToString());
+            PrintRow("Square", OrderedBlocks[0].NumberOfRedShape.ToString(), OrderedBlocks[0].NumberOfBlueShape.ToString(), OrderedBlocks[0].NumberOfYellowShape.ToString());
+            PrintRow("Triangle", OrderedBlocks[1].NumberOfRedShape.ToString(), OrderedBlocks[1].NumberOfBlueShape.ToString(), OrderedBlocks[1].NumberOfYellowShape.ToString());
+            PrintRow("Circle", OrderedBlocks[2].NumberOfRedShape.ToString(), OrderedBlocks[2].NumberOfBlueShape.ToString(), OrderedBlocks[2].NumberOfYellowShape.ToString());
             PrintLine(new string('-', tableWidth));
         }
         //Consider moving this to the base class 
