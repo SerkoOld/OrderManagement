00e0b7ef90fbfdf4dcef530a1c0cc420803b255d
Jerry Li <jerryauckland@gmail.com>
2021-08-20 20:15:39 +1200

    Clone solution for Code Review

---
+++ Order.Management.PR/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Management
+{
+    class Circle : Shape
+    {
+        public int circlePrice = 3;
+        public Circle(int red, int blue, int yellow)
+        {
+            Name = "Circle";
+            base.Price = circlePrice;
+            AdditionalCharge = 1;
+            base.NumberOfRedShape = red;
+            base.NumberOfBlueShape = blue;
+            base.NumberOfYellowShape = yellow;
+        }
+        public override int Total()
+        {
+            return RedCirclesTotal() + BlueCirclesTotal() + YellowCirclesTotal();
+        }
+
+        public int RedCirclesTotal()
+        {
+            return (base.NumberOfRedShape * Price);
+        }
+        public int BlueCirclesTotal()
+        {
+            return (base.NumberOfBlueShape * Price);
+        }
+        public int YellowCirclesTotal()
+        {
+            return (base.NumberOfYellowShape * Price);
+        }
+    }
+}
---
+++ Order.Management.PR/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Management
+{
+    abstract class Order
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string DueDate { get; set; }
+        public int OrderNumber { get; set; }
+        public List<Shape> OrderedBlocks { get; set; }
+
+        public abstract void GenerateReport();
+
+        public string ToString()
+        {
+            return "\nName: " + CustomerName + " Address: " + Address + " Due Date: " + DueDate + " Order #: " + OrderNumber;
+        }
+    }
+}
